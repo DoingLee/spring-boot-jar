f3188afc316e8ce09fe31edd4cc6d49e0e310f40
lidongying <lidongying@corp.netease.com>
2017-10-18 21:52:09 +0800

    在spring-boot中使用xml配置bean

---
+++ src/main/java/cn/doing/bean/BeanConfig.java
@@ -0,0 +1,15 @@
+package cn.doing.bean;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+/**
+ * classpath路径：locations={"classpath:application-bean1.xml","classpath:application-bean2.xml"}
+ * file路径： locations ={"file:d:/test/application-bean1.xml"};
+ */
+
+@Configuration
+@ImportResource(locations = {"classpath:spring/application-bean.xml"})
+public class BeanConfig {
+
+}
---
+++ src/main/java/cn/doing/bean/TestBean2.java
@@ -0,0 +1,7 @@
+package cn.doing.bean;
+
+public class TestBean2 {
+    public TestBean2() {
+        System.out.println("TestBean2 自动被Spring容器加载！");
+    }
+}
